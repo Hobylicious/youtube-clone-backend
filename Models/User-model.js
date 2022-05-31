7a8c5b7fcc00c501fb80d0d5d47fffd51b9a5660
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-05-30 20:16:58 -0400

    .

--- Models/User-model.js
+++ Models/User-model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 // const passportLocalMongoose = require('passport-local-mongoose');
 
 
-var User = new Schema({  
+const User = new Schema({  
 
     username: {
         type:String,
